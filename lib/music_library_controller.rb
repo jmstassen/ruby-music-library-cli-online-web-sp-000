f7b5acaaee4ab4d273b4a55f5b60fd88e053467d
John Stassen <john.stassen@gmail.com>
2020-08-30 20:56:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,7 @@ class MusicLibraryController
       artist_songs_alphabetized = artist_requested.songs.sort_by {|song| song.name}
       list_number = 1
       artist_songs_alphabetized.each do |song|
-        puts "#{list_number}. #{song.name}"
+        puts "#{list_number}. #{song.name} - #{song.genre}"
         list_number += 1
       end
     else
