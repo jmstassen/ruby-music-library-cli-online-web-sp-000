4658a16c2f8ff371175b4db603f03c986ba371e8
John Stassen <john.stassen@gmail.com>
2020-08-30 21:25:29 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -91,7 +91,7 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    # list_songs
+    list_songs
     choice = gets.chomp
     # if choice.to_i.between?(1, @alphabetized_song_all.count)
     puts "Playing #{@alphabetized_song_all[choice.to_i - 1].name} by #{@alphabetized_song_all[choice.to_i - 1].artist.name}"
